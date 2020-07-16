5f17442f2cb15077fbf07a1d68e227228ea8506f
zhaoshuai <314253127@qq.com>
2020-07-16 21:48:03 +0800

    add ps1

---
+++ math/sonar.ps1
@@ -0,0 +1,5 @@
+﻿dotnet test Math.Test/Math.Test.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+
+dotnet sonarscanner begin /k:"sonardotnetcoveragetest" /d:sonar.host.url="http://172.29.21.211" /d:sonar.login="6cb73b36f9fc21f7d58948fb09f833f28dcab9ec"
+dotnet build
+dotnet sonarscanner end /d:sonar.login="6cb73b36f9fc21f7d58948fb09f833f28dcab9ec"
