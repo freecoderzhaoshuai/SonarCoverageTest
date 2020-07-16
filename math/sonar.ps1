06b23a2d7d2c86537cc51cdadcc7c4ca03cb99f8
zhaoshuai <314253127@qq.com>
2020-07-16 22:08:15 +0800

    add coverage

--- math/sonar.ps1
+++ math/sonar.ps1
@@ -1,5 +1,5 @@
 ﻿dotnet test Math.Test/Math.Test.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
-
-dotnet sonarscanner begin /k:"sonardotnetcoveragetest" /d:sonar.host.url="http://172.29.21.211" /d:sonar.login="6cb73b36f9fc21f7d58948fb09f833f28dcab9ec"
-dotnet build
+dotnet build-server shutdown
+dotnet sonarscanner begin /k:"sonardotnetcoveragetest" /d:sonar.host.url="http://172.29.21.211" /d:sonar.login="6cb73b36f9fc21f7d58948fb09f833f28dcab9ec" /d:sonar.cs.opencover.reportsPaths=Math.Test/coverage.opencover.xml  /d:sonar.coverage.exclusions="**Test*.cs"
+dotnet sonarscanner begin /k:"sonardotnetcoveragetest" /d:sonar.host.url="http://172.29.21.211" /d:sonar.login="6cb73b36f9fc21f7d58948fb09f833f28dcab9ec" /d:sonar.cs.opencover.reportsPaths=Math.Test/coverage.opencover.xml  /d:sonar.coverage.exclusions="**Test*.cs"
 dotnet sonarscanner end /d:sonar.login="6cb73b36f9fc21f7d58948fb09f833f28dcab9ec"
